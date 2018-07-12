c8a756c4f3f5309061a80e8c5201f1a658e996ea
AndresSal <andres.salazar01@epn.edu.ec>
2018-07-12 09:32:26 -0500

    creación del modelo y controlador de una materia

---
+++ Examen-IIB-Back/api/models/Materia.js
@@ -0,0 +1,42 @@
+/**
+ * Materia.js
+ *
+ * @description :: A model definition.  Represents a database table/collection/etc.
+ * @docs        :: https://sailsjs.com/docs/concepts/models-and-orm/models
+ */
+
+module.exports = {
+
+  attributes: {
+    idMateria : {
+      type: 'number',
+      autoIncrement: true
+    },
+    nombreMateria : {
+      type: 'string',
+      required: true
+    },
+    codigoMateria : {
+      type: 'string',
+      required: false
+    },
+    descripcionMateria : {
+      type: 'string',
+      required: false
+    },
+    esDisponible : {
+      type: 'boolean',
+      required: true
+    },
+    fechaCreacion : {
+      type: 'string',
+      required: false
+    },
+    numHorasXSemana : {
+      type: 'number',
+      required: false
+    }
+  },
+
+};
+
